c827985668ff2808920a0447e9ba4dbbbebf76b9
Max <bitveen@gmail.com>
2017-01-05 19:47:12 +0300

    Created sse handler

--- public/client.js
+++ public/client.js
@@ -1,1 +1,37 @@
-console.log("Ok");
+(function() {
+
+    // область для вставки контента
+    const logsArea = document.getElementById('logs-area');
+    const source = new EventSource('/watch');
+
+    source.addEventListener('change', handleEvent, false);
+    source.addEventListener('rename', handleEvent, false);
+
+
+    function handleEvent(e) {
+        let data = e.data.split('\n');
+        let timestamp = data[0].split(' ')[1];
+        let filename = data[1].split(' ')[1];
+
+        logsArea.innerHTML += `<tr>
+                                <td>${e.type}</td>
+                                <td>${timestamp}</td>
+                                <td>${filename}</td>
+                               </tr>`;
+    }
+
+
+    // source.addEventListener('open', (e) => {
+    //     // вывести что подключение установлено
+    // }, false);
+    //
+    //
+    //
+    // source.addEventListener('error', (e) => {
+    //     if (e.readyState === EventSource.CLOSED) {
+    //         // подключение закрыто
+    //     }
+    // });
+
+
+})();
--- server.js
+++ server.js
@@ -1,118 +1,40 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const fs = require('fs');
-const path = require('path');
 
-const viewsPath = '/views';
-
-
+const WATCH_FOLDER = __dirname + '/src';
+const PORT = 8080;
 
 
 
-function handleBadRequest(req, res) {
-    let filePromise = new Promise((resolve, reject) => {
-        let errTemplatePath = path.join(__dirname, viewsPath, '/error.html');
-        fs.readFile(errTemplatePath, (err, fileContent) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(fileContent);
-        });
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+
+app.get('/watch', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
     });
 
-    filePromise.then((fileContent) => {
-        res.writeHead(400, {
-            'Content-Type': 'text/html',
-            'Content-Length': Buffer.byteLength(fileContent)
-        });
-        res.end(fileContent.toString('utf-8'));
-    }, (err) => {
-        console.error(err);
-        res.end();
+    const params = {
+        persistent: true,
+        recursive: true,
+        encoding: 'utf8'
+    };
+
+    fs.watch(WATCH_FOLDER, params, (eventType, filename) => {
+        let data = `event:${eventType}\ndata:timestamp ${Date.now()}\ndata:filename ${filename}\n\n`;
+        res.write(data);
     });
 
-
-}
-
-
-function handleNotFound(req, res) {
-    let filePromise = new Promise((resolve, reject) => {
-        let notFoundTemplatePath = path.join(__dirname, viewsPath, '/404.html');
-        fs.readFile(notFoundTemplatePath, (err, fileContent) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(fileContent);
-        });
-    });
-
-    filePromise.then((fileContent) => {
-        res.writeHead(404, {
-            'Content-Type': 'text/html',
-            'Content-Length': Buffer.byteLength(fileContent)
-        });
-        res.end(fileContent.toString('utf-8'));
-    }, (err) => {
-        console.error(err);
-        res.end();
-    });
-
-}
+});
 
 
-function requestHandler(req, res) {
-
-    if (req.method !== 'GET') {
-        handleBadRequest(req, res);
-    }
-
-    switch (req.url) {
-        case '/':
-            fs.readFile(path.join(__dirname, viewsPath, '/index.html'), (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                res.writeHead(200, {
-                    'Content-Type': 'text/html',
-                    'Content-Length': Buffer.byteLength(data)
-                });
-                res.end(data.toString('utf-8'));
-            });
-            break;
-            // For Google Chrome
-        case '/favicon.ico':
-            res.statusCode = 404;
-            res.statusMessage = 'Not found';
-            res.end();
-            break;
-        case '/public/client.js':
-            fs.readFile(path.join(__dirname, '/public/client.js'), (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                res.writeHead(200, {
-                    'Content-Type': 'application/javascript',
-                    'Content-Length': Buffer.byteLength(data)
-                });
-                res.end(data.toString('utf-8'));
-            });
-            break;
-        case '/watch':
-            // sse
-
-            break;
-
-        default:
-            handleNotFound(req, res);
-    }
-
-
-
-}
-
-
-
-
-
-
-
-http.createServer(requestHandler).listen(process.env.PORT || 8080, () => console.log('Server listening...'));
+app.listen(PORT, () => console.log('Server listening...'));
